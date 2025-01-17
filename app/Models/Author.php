fae93871a01f6b817865328b1ec6246f5a451f15
Ari P <ariteknologi@gmail.com>
2025-01-17 12:22:23 +0700

    add author crud

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use Backpack\CRUD\app\Models\Traits\CrudTrait;
 use Illuminate\Database\Eloquent\Model;
 
 class Author extends Model
 {
+    use CrudTrait;
     protected $table = 'authors';
 
     protected $fillable = [
